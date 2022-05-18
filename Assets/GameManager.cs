051018e4ac2210d01160eaaaef3c492d24eadacb
Valey <35815374+ValeyCplusplus@users.noreply.github.com>
2022-05-18 18:07:08 +0200

    GameManager added

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    
+    }
+}
